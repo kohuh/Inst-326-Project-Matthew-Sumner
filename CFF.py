dc2e4d83b46466e269aa2f735c90dbc0f641bf81
kohuh <bobseny90@gmail.com>
2026-05-06 12:41:03 -0400

    Update CFF.py
    Created basis for class: FlightSearch users will be able to input departure ids, arrival ids, minimum price, max price, and the date they want to fly.
    
    Added Init function and 1 class function

--- CFF.py
+++ CFF.py
@@ -1,17 +1,28 @@
 #Cheap, Financial Flights
 from serpapi import GoogleSearch
 import pandas as pd
+import json
 
-param = {
-    "engine": "google_flights",
-    "departure_id": "JFK",          
-    "arrival_id": "LAX",         
-    "outbound_date": "2026-06-15",  
-    "currency": "USD",
-    "hl": "en",
-    "api_key": "65aadbc33f7564338658dd6568a7b3e2dcc100ebe47fa43f51dbcf03beda75b6"
-}
-
-search = GoogleSearch(param)
-
+class FlightSearch:
+    def __init__(self, departure, arrival, min_price, max_price, outbound_date):
+        if min_price<=0 or max_price<=0:
+            raise ValueError("Prices must exceed 0")
+        elif min_price>max_price:
+            raise ValueError("Minimum price must be less than or equal to max price")
+        self.departure=departure
+        self.arrival=arrival
+        self.min_price=min_price
+        self.max_price=max_price
+        self.outbound_date=outbound_date
+        param = {
+            "engine": "google_flights", "departure_id": self.departure, "arrival_id": self.arrival, "outbound_date": self.outbound_date, "currency": "USD", "hl": "en",
+            "api_key": "65aadbc33f7564338658dd6568a7b3e2dcc100ebe47fa43f51dbcf03beda75b6", "sort_by":2, "max_price":self.max_price
+        }
+        search = GoogleSearch(param)  
+    def convertToList(self, dictionary):
+        temp=json.loads(dictionary)
+        flightList=[]
+        for i in temp:
+            
+            
 #Potential use case for using Pandas `DataFrame` to store and analyze flight data, including prices, airlines, and departure times.
