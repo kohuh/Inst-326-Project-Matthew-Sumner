4580e8b7a0079ec17e4666a10a733edda8e65cca
sumslayz <sbergenfield@gmail.com>
2026-05-07 12:18:22 -0400

    Update CFF.py
    Working on how new information will be stored via csv file/pandas file

--- CFF.py
+++ CFF.py
@@ -16,15 +16,32 @@ class FlightSearch:
         self.outbound_date=outbound_date
         param = {
             "engine": "google_flights", "departure_id": self.departure, "arrival_id": self.arrival, "outbound_date": self.outbound_date, "currency": "USD", "hl": "en",
-            "api_key": "65aadbc33f7564338658dd6568a7b3e2dcc100ebe47fa43f51dbcf03beda75b6", "sort_by":2, "max_price":self.max_price, "type": 2
+            "api_key": "2fa1e48f28a2949f47a62f717b2b70e051afb251b0abf77717c5c9462b155c7f", "sort_by":2, "max_price":self.max_price, "type": 2
         }
         search = GoogleSearch(param)
         self.convertToList(search)
     def convertToList(self, dictionary):
         temp=json.loads(dictionary)
         flightList=[]
-        for i in temp:
-            
+        # for i in temp:
+
+    def storeData(self, flightList):
+        """
+        Stores flight data in a CSV file using Pandas DataFrame.
+        """
+        df = pd.DataFrame(flightList, columns=['Airline', 'Price', 'Departure Time', 'Arrival Time'])
+        df.to_csv('flights.csv', index=False)
+
+
+
+if __name__ == "__main__":
+    departure = input("Enter departure location: (i.e., JFK) ")
+    arrival = input("Enter arrival location: (i.e., LAX) ")
+    min_price = float(input("Enter minimum price: "))
+    max_price = float(input("Enter maximum price: "))
+    outbound_date = input("Enter outbound date (YYYY-MM-DD): ")
+
+    flight_search = FlightSearch(departure, arrival, min_price, max_price, outbound_date)
 
 
             
