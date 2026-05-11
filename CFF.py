955cf50ed6baec0c68f909a311e8af79c7e232db
kohuh <bobseny90@gmail.com>
2026-05-10 21:58:20 -0400

    Update CFF.py
    Finished completion of flight class objects. Added __lt__ for use in sorting function added sort function and __repr__

--- CFF.py
+++ CFF.py
@@ -5,54 +5,57 @@ import json
 import os
 
 class FlightSearch:
-    def __init__(self, departure, arrival, min_price, max_price, outbound_date):
+    def __init__(self, departure, arrival, outbound_date):
         self.departure=departure
         self.arrival=arrival
-        self.min_price=min_price
-        self.max_price=max_price
         self.outbound_date=outbound_date
         #self.api_key = os.getenv("api_key")
-        self.api_key = os.getenv("3d39eb189e17e631e1448aaa9fe368fc52344f30a2a3ad937f26f066ceec1b52")
+        self.api_key = "3d39eb189e17e631e1448aaa9fe368fc52344f30a2a3ad937f26f066ceec1b52"
         param = {
             "engine": "google_flights", "departure_id": self.departure, "arrival_id": self.arrival, "outbound_date": self.outbound_date, "currency": "USD", "hl": "en",
-            "api_key": self.api_key, "sort_by":2, "max_price":self.max_price, "type": 2
+            "sort_by":2, "type": 2
         }
         client = serpapi.Client(api_key=self.api_key)
         search = client.search(param)
-        self.combThrough(search["best"])
-    
+        all_itineraries = search.get("best_flights", []) + search.get("other_flights", [])
+        self.flightList=self.createList(all_itineraries) #creates list of flight objects
+
     def sortByPrice(self):
-        if self.min_price<=0 or self.max_price<=0:
+        return sorted(self.flightList)
+    def setPriceRange(self, max_price, min_price=0):
+        priced_list=[]
+        if min_price<=0 or max_price<=0:
             raise ValueError("Prices must exceed 0")
-        elif self.min_price>self.max_price:
+        elif min_price>max_price:
             raise ValueError("Minimum price must be less than or equal to max price")
-        
+        else:
+            for flight in self.flightList:
+                if flight.price>min_price and flight.price<max_price:
+                    priced_list.append(flight)
+        return priced_list
+    
+    def printList(self):
+        for i in self.flightList:
+            print(i) 
+            print("\n")
 
-    def combThrough(self, inputs):
+    def createList(self, input):
         flight_list=[]
-        flights=json.loads
-        for t in flights:
-            flights={}
-            # Extract relevant flight information
-            #... (Extract other relevant flight information such as price, departure time, etc.)
-
-
-
-
-    def storeData(self, flightList):
-        """
-        Stores flight data in a CSV file using Pandas DataFrame.
-        """
-        df = pd.DataFrame(flightList, columns=['Airline', 'Price', 'Departure Time', 'Arrival Time'])
-        df.to_csv('flights.csv', index=False)
-        print(df)
+        for t in input:
+            first_leg=t["flights"][0]
+            last_leg=t["flights"][-1]
+            has_layover = len(t.get("layovers", [])) > 0
+            flight_list.append(Flight(first_leg["flight_number"], first_leg["airline"], t["price"], first_leg["departure_airport"]["time"], last_leg["arrival_airport"]["time"], duration=t["total_duration"], layover=has_layover))
+        return flight_list
 
 class Flight():
-    def __init__(self, airline, price, departure_time, arrival_time, duration=0, layover=False):
+    def __init__(self, flight_number, airline, price, departure_time, arrival_time, duration=0, layover=False,):
+        self.flight_number=flight_number
         self.airline=airline 
         self.price=price 
         self.departure_time=departure_time
         self.arrival_time=arrival_time
+        self.duration=duration
         self.layover=layover
     def __lt__(self, other):
         if isinstance(other, Flight):
@@ -60,6 +63,8 @@ class Flight():
                 return True
             else:
                 return False
+        else:
+            raise ValueError("Object Other is not Instance of Flight object")
     def __eq__(self, other):
         if isinstance(other, Flight):
             if other.price==self.price:
@@ -68,11 +73,14 @@ class Flight():
                 return False
         else:
             raise ValueError("Object Other is not Instance of Flight object")
-
+    def __str__(self):
+        return f"airline flight number {self.flight_number} on {self.airline}, for {self.price}, leaving at {self.departure_time}, and arriving at {self.arrival_time}, for a total of {self.duration}"
+    def __repr__(self):
+        return f"({self.flight_number}, {self.airline}, {self.price}, {self.departure_time}, {self.arrival_time}, {self.duration}, {self.layover}"
 if __name__ == "__main__":
-    departure = input("Enter departure location: (i.e., JFK) ")
-    arrival = input("Enter arrival location: (i.e., LAX) ")
-    min_price = float(input("Enter minimum price: $"))
-    max_price = float(input("Enter maximum price: $"))
-    outbound_date = input("Enter outbound date (YYYY-MM-DD): ")
-    flight_search = FlightSearch(departure, arrival, min_price, max_price, outbound_date)
+    #departure = input("Enter departure location: (i.e., JFK) ")
+    #arrival = input("Enter arrival location: (i.e., LAX) ")
+    #outbound_date = input("Enter outbound date (YYYY-MM-DD): ")
+    fs = FlightSearch("CDG", "AUS", "2026-09-12")
+    fs.printList()
+
