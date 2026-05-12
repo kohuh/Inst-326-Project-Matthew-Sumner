7179cccdf82cec6c46d28808f7ddaf9c1d424338
sumslayz <sbergenfield@gmail.com>
2026-05-12 07:18:42 -0400

    Update on test_CFF.py
    added reasons why we added these specific tests for the project.

--- test_CFF.py
+++ test_CFF.py
@@ -2,6 +2,7 @@ from CFF import FlightSearch, Flight
 
 
 # Unit test for __init__ function
+# This is meant to test whether the paramaters for the FlightSearch class are set up correctly.
 fs = FlightSearch("CDG", "AUS", "2026-09-12")
 assert fs.departure == "CDG", "Departure airport code not set correctly"
 assert fs.arrival == "AUS", "Arrival airport code not set correctly"
@@ -11,10 +12,14 @@ assert isinstance(fs.flightList, list), "Flight list not created correctly"
 ## class FlightSearch
 
 # Unit test for sortByPrice function
+# This is meant to test whether the sortByPrice function is correctly sorting the flight objects 
+# in price order.
 sorted_flights = fs.sortByPrice()
 assert sorted_flights == sorted(fs.flightList), "Flights not sorted correctly by price"
 
-#Unit test for setPriceRange function
+# Unit test for setPriceRange function
+# This is meant to test whether the setPriceRange function is filtering the flight objects by a 
+# specific price range.
 expensive_flight = fs.setPriceRange(1000, 500)
 for flight in expensive_flight:
     assert flight.price >= 500 and flight.price <= 1000, "Flight price is not within the specified price range"
@@ -36,6 +41,8 @@ fs.printList(), "prints list of flight objects in readable format"
 ## class Flight
 
 #Unit test for __init__ function
+# This is meant to test whether the pramaters for Flight class return the correct attributes for the
+# flight object.
 flight = Flight("AA755", "American Airlines", 781.0, "2026-09-12 13:55:00", "2026-09-12 20:27:00", 812, True)
 assert flight.flight_number == "AA755", "Flight number not set correctly"
 assert flight.airline == "American Airlines", "Airline not set correctly"
@@ -46,16 +53,24 @@ assert flight.duration == 812, "Duration not set correctly"
 assert flight.layover == True, "Layover not True when there is a layover"
 
 #Unit test for __lt__ function
+# This is meant to test whether the __lt__ function is actually comparing the price the two flight objects
+# and returning the associated boolean value
 flight1 = Flight("AA755", "American Airlines", 781.0, "2026-09-12 13:55:00", "2026-09-12 20:27:00", 812, True)
 flight2 = Flight("BA305", "British Airways", 801.0, "2026-09-12 14:15:00", "2026-09-12 20:35:00", 800, False)
 assert flight1.__lt__(flight2) == True, "Flight 1 should be less than Flight 2 based on price"
 assert flight2.__lt__(flight1) == False, "Flight 2 should not be less than Flight 1 based on price"
 
 #Unit test for __eq__ function
+# This is meant to test if the __eq__ function is comparing the price of two flight objects and returning
+# the correct boolean value whether the condition is met.
 assert flight1.__eq__(flight2) == False, "Flight 1 should not be equal to Flight 2 based on price"
 
 #Unit test for __str__ function
+# This is mean to test whether the __str__ function is returning a correct string representaion of the flight
+# object.
 assert flight.__str__() == "airline flight number AA755 on American Airlines, for $781.0 dollars, leaving at 2026-09-12 13:55:00, and arriving at 2026-09-12 20:27:00, for a total of 812 minutes.", "String representation of flight object not correct"
 
 #Unit test for __repr__ function
+# This is meant to test whether the __repr__ function is returning the correct __repr__ represenation of the
+# flight object.
 assert flight.__repr__() == "(AA755, American Airlines, 781.0, 2026-09-12 13:55:00, 2026-09-12 20:27:00, 812, True.)", "Repr representation of flight object not correct"
